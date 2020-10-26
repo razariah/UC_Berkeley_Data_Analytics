52c37f4ff9c54b50792a49933a00efff719738ee
Ramesh Azariah <ramesh.azariah@gmail.com>
2020-10-25 22:58:01 -0700

    Add files via upload

---
+++ 13_Mapping_Earthquakes_with_JS_and_APIs/mapbox-practice/config.js
@@ -0,0 +1,1 @@
+const API_KEY = 'pk.eyJ1IjoiYW5kcmV3aG9hbmcwOSIsImEiOiJjamt1Zno2ejcwNTFkM3FwZGJrOXk1bWxxIn0.BCVeyxRcjhsbOLVqnx5uTQ'
---
+++ 13_Mapping_Earthquakes_with_JS_and_APIs/mapbox-practice/index.js
@@ -0,0 +1,9 @@
+
+mapboxgl.accessToken = API_KEY;
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-122.41,37.77], // lng lat
+    zoom: 9
+})
+
